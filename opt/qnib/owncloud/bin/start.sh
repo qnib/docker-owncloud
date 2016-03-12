f94fb21be21264edb4e30a96163d133b39986adb
Christian Kniep <christian@qnib.org>
2016-03-12 11:29:45 +0100

    chown of nginx-tempdir

--- opt/qnib/owncloud/bin/start.sh
+++ opt/qnib/owncloud/bin/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/local/bin/dumb-init /bin/bash
 
 adduser -h /var/lib/owncloud/data/ -s /sbin/nologin -u ${OC_UID} -D owncloud
 mkdir -p /var/log/owncloud
@@ -9,6 +9,9 @@ chown -R owncloud: /etc/owncloud/
 # Data
 mkdir -p /data/owncloud/
 chown -R owncloud: /data/owncloud/ /usr/share/webapps/owncloud/config /usr/share/webapps/owncloud/apps/ 
+
+# Nginx temp
+chown owncloud: -R /var/lib/nginx/
 
 consul-template -consul localhost:8500 -once -template "/etc/consul-templates/owncloud/owncloud.conf.ctmpl:/etc/nginx/conf.d/owncloud.conf"
 
