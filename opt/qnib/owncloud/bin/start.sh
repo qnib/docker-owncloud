698d7bd8e86902e2ccf1773c2372d2d47a6c0589
Christian Kniep <christian@qnib.org>
2016-03-26 17:55:33 +0100

    use root within the container

--- opt/qnib/owncloud/bin/start.sh
+++ opt/qnib/owncloud/bin/start.sh
@@ -1,20 +1,19 @@
 #!/usr/local/bin/dumb-init /bin/bash
 
-adduser -h /var/lib/owncloud/data/ -s /sbin/nologin -u ${OC_UID} -D owncloud
+#adduser -s /sbin/nologin -u ${OC_UID} -D owncloud
 mkdir -p /var/log/owncloud
 
-chown owncloud: /var/log/owncloud
+#chown owncloud: /var/log/owncloud
 
-chown -R owncloud: /etc/owncloud/
+#chown -R owncloud: /etc/owncloud/
 # Data
 mkdir -p /data/owncloud/
-chown -R owncloud: /data/owncloud/ /usr/share/webapps/owncloud/config /usr/share/webapps/owncloud/apps/ 
+#chown -R owncloud: /data/owncloud/ /usr/share/webapps/owncloud/config /usr/share/webapps/owncloud/apps/ 
 
 # Nginx temp
-chown owncloud: -R /var/lib/nginx/
+#chown owncloud: -R /var/lib/nginx/
 
 consul-template -consul localhost:8500 -once -template "/etc/consul-templates/owncloud/owncloud.conf.ctmpl:/etc/nginx/conf.d/owncloud.conf"
-
 
 nginx
 
